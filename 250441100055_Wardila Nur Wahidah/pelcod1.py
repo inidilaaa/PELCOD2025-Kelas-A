48841f58880b1a1c575a3b7c338d56334fd3fa07
inidila <dilawahidah44@gmail.com>
2025-09-19 11:56:40 +0700

    250441100055_Wardila Nur Wahidah

---
+++ 250441100055_Wardila Nur Wahidah/pelcod1.py
@@ -0,0 +1,37 @@
+print("="*50)
+print("Nama saya dila")
+print("="*50)
+nama = "dila"
+print(nama)
+print(type(nama))
+boolean = True
+print(type(boolean))
+
+penjumlahan = 5+7
+print("hasilnya adalah", penjumlahan)
+
+nama_asprak = "dila preketek"
+nama_calon_dosen = nama_asprak
+print(nama_calon_dosen)
+
+# tipe data number
+
+# tahun_lahir = int(input("masukkan tahun lahir : "))
+# tahun_sekarang = int(input("masukkan tahun sekarang : "))
+# perhitungan_usia = tahun_sekarang - tahun_lahir
+# print(perhitungan_usia)
+
+# bilangan_1 = 51
+# bilangan_2 = 7
+
+# modulus = bilangan_1 % bilangan_2
+# print(modulus)
+
+# cek = bilangan_1 < bilangan_2
+# print(cek)
+
+saldo = float(input("masukkan saldo awal : "))
+deposit = float(input("masukkan saldo deposit : "))
+
+saldo += deposit
+print(saldo)
